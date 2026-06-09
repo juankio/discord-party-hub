ecd5f4053a362f187dfcc1772c7bc30ad2ba8f78
Miguel <juanmiguelruizsupe@gmail.com>
2026-06-09 17:38:23 -0500

    style(lobby): drastically reduce height of game selector shelves to make them compact and proportional

--- components/GameSelector.vue
+++ components/GameSelector.vue
@@ -3,7 +3,7 @@
     <div class="min-w-[340px] flex flex-col">
       <div
   v-for="(row, idx) in [games.slice(0,3), games.slice(3,5)]" :key="idx" 
-           class="relative w-full flex justify-center gap-4 sm:gap-8 items-end pb-[24px] pt-20 sm:pt-24 bg-[#382012]">
+           class="relative w-full flex justify-center gap-4 sm:gap-8 items-end pb-[24px] pt-12 sm:pt-16 bg-[#382012]">
         
         <!-- Soga -->
         <div class="absolute left-0 right-0 h-[10px] z-[25] pointer-events-none" style="bottom: 50px;">
