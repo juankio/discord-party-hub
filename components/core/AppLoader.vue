9cb6a666894027945e8b176d7924875aaca6995c
Miguel <juanmiguelruizsupe@gmail.com>
2026-06-11 15:42:23 -0500

    style(loader): fix 3d flattening context for dice bouncing animation

--- components/core/AppLoader.vue
+++ components/core/AppLoader.vue
@@ -5,19 +5,21 @@
 
     <!-- 3D Dice Container -->
     <div class="dice-wrapper relative w-32 h-32 flex items-center justify-center perspective-1000 mb-8 mt-12 z-10">
-      <div class="dice w-16 h-16 relative transform-style-3d">
-        <!-- 1 -->
-        <div class="face front bg-rose-500"><div class="w-full h-full flex items-center justify-center"><div class="dot w-3 h-3"></div></div></div>
-        <!-- 2 -->
-        <div class="face back bg-sky-500"><div class="w-full h-full flex justify-between p-2"><div class="dot self-start"></div><div class="dot self-end"></div></div></div>
-        <!-- 3 -->
-        <div class="face right bg-amber-400"><div class="w-full h-full flex flex-col justify-between p-1.5"><div class="dot self-start"></div><div class="dot self-center"></div><div class="dot self-end"></div></div></div>
-        <!-- 4 -->
-        <div class="face left bg-emerald-500"><div class="w-full h-full flex flex-col justify-between p-2"><div class="flex justify-between"><div class="dot"></div><div class="dot"></div></div><div class="flex justify-between"><div class="dot"></div><div class="dot"></div></div></div></div>
-        <!-- 5 -->
-        <div class="face top bg-violet-500"><div class="relative w-full h-full p-2"><div class="dot absolute top-2 left-2"></div><div class="dot absolute top-2 right-2"></div><div class="dot absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div><div class="dot absolute bottom-2 left-2"></div><div class="dot absolute bottom-2 right-2"></div></div></div>
-        <!-- 6 -->
-        <div class="face bottom bg-orange-500"><div class="w-full h-full flex flex-col justify-between p-2"><div class="flex justify-between"><div class="dot"></div><div class="dot"></div></div><div class="flex justify-between"><div class="dot"></div><div class="dot"></div></div><div class="flex justify-between"><div class="dot"></div><div class="dot"></div></div></div></div>
+      <div class="dice-bounce w-16 h-16 relative transform-style-3d">
+        <div class="dice-spin w-full h-full relative transform-style-3d">
+          <!-- 1 -->
+          <div class="face front bg-rose-500"><div class="w-full h-full flex items-center justify-center"><div class="dot w-3 h-3"></div></div></div>
+          <!-- 2 -->
+          <div class="face back bg-sky-500"><div class="w-full h-full flex justify-between p-2"><div class="dot self-start"></div><div class="dot self-end"></div></div></div>
+          <!-- 3 -->
+          <div class="face right bg-amber-400"><div class="w-full h-full flex flex-col justify-between p-1.5"><div class="dot self-start"></div><div class="dot self-center"></div><div class="dot self-end"></div></div></div>
+          <!-- 4 -->
+          <div class="face left bg-emerald-500"><div class="w-full h-full flex flex-col justify-between p-2"><div class="flex justify-between"><div class="dot"></div><div class="dot"></div></div><div class="flex justify-between"><div class="dot"></div><div class="dot"></div></div></div></div>
+          <!-- 5 -->
+          <div class="face top bg-violet-500"><div class="relative w-full h-full p-2"><div class="dot absolute top-2 left-2"></div><div class="dot absolute top-2 right-2"></div><div class="dot absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div><div class="dot absolute bottom-2 left-2"></div><div class="dot absolute bottom-2 right-2"></div></div></div>
+          <!-- 6 -->
+          <div class="face bottom bg-orange-500"><div class="w-full h-full flex flex-col justify-between p-2"><div class="flex justify-between"><div class="dot"></div><div class="dot"></div></div><div class="flex justify-between"><div class="dot"></div><div class="dot"></div></div><div class="flex justify-between"><div class="dot"></div><div class="dot"></div></div></div></div>
+        </div>
       </div>
       
       <!-- Shadow -->
@@ -62,7 +64,7 @@ let textInterval: ReturnType<typeof setInterval> | null = null
 onMounted(() => {
   // Dice bouncing animation
   anime({
-    targets: '.dice',
+    targets: '.dice-bounce',
     translateY: [0, -70],
     easing: 'easeOutCubic',
     duration: 500,
@@ -72,10 +74,10 @@ onMounted(() => {
 
   // Dice tumbling/spinning animation
   anime({
-    targets: '.dice',
-    rotateX: '+=360',
-    rotateY: '+=180',
-    rotateZ: '+=90',
+    targets: '.dice-spin',
+    rotateX: '+=360deg',
+    rotateY: '+=180deg',
+    rotateZ: '+=90deg',
     easing: 'linear',
     duration: 3000,
     loop: true
@@ -110,7 +112,8 @@ onMounted(() => {
 
 onUnmounted(() => {
   if (textInterval) clearInterval(textInterval)
-  anime.remove('.dice')
+  anime.remove('.dice-bounce')
+  anime.remove('.dice-spin')
   anime.remove('.dice-shadow')
   anime.remove('.loading-bar')
 })
